3c918fd49b8dadd5ae442f24b83b15fa85978d6d
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-04-28 15:56:13 -0400

    basic screens & bottom tabs navigation

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StackNavigation from './StackNavigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <>
+      <StackNavigation />
       <StatusBar style="auto" />
-    </View>
+    </>
   );
 }
 
