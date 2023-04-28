6696adf7bc2e41630f10ca228b8e136e36d96bc5
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-04-28 15:32:38 -0400

    basic navigation

---
+++ StackNavigation.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const StackNavigation = () => {
+  return (
+	<View>
+	  <Text>StackNavigation</Text>
+	</View>
+  )
+}
+
+export default StackNavigation
+
+const styles = StyleSheet.create({})
