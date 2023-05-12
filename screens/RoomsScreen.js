af86f2df5b2db8bfbed725a488bdaf99ad2608d0
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-05-12 12:02:26 -0400

    rooms screen finished

--- screens/RoomsScreen.js
+++ screens/RoomsScreen.js
@@ -6,6 +6,7 @@ import { ScrollView } from "react-native";
 import { Pressable } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import Amenities from "../components/Amenities";
+import { Entypo } from "@expo/vector-icons";
 
 const RoomsScreen = () => {
   const route = useRoute();
@@ -31,91 +32,146 @@ const RoomsScreen = () => {
     });
   }, []);
 
-  const [selected,setSelected] = useState([])
+  const [selected, setSelected] = useState([]);
 
   return (
-    <ScrollView>
-      {route.params.rooms.map((item, index) => (
-        <Pressable
-          style={{
-            margin: 10,
-            backgroundColor: "white",
-            padding: 10,
-          }}
-          key={index}
-        >
-          <View
-            style={{
-              flexDirection: "row",
-              alignItems: "center",
-              justifyContent: "space-between",
-            }}
-          >
-            <Text
-              style={{
-                color: "#007fff",
-                fontSize: 17,
-                fontWeight: "500",
-              }}
-            >
-              {item.name}
-            </Text>
-            <AntDesign name="infocirlceo" size={24} color={"#007fff"} />
-          </View>
-
-          <Text style={{ marginTop: 3 }}>pay at property</Text>
-          <Text style={{ marginTop: 3, color: "green", fontSize: 16 }}>
-            Free cancellation Available
-          </Text>
-          <View
-            style={{
-              marginTop: 4,
-              flexDirection: "row",
-              alignItems: "center",
-              gap: 10,
-            }}
-          >
-            <Text
-              style={{
-                fontSize: 16,
-                color: "red",
-                textDecorationLine: "line-through",
-              }}
-            >
-              Rs{route.params.oldPrice}
-            </Text>
-            <Text
-              style={{
-                fontSize: 18,
-              }}
-            >
-              Rs{route.params.newPrice}
-            </Text>
-          </View>
-          <Amenities />
-
+    <>
+      <ScrollView>
+        {route.params.rooms.map((item, index) => (
           <Pressable
             style={{
-              borderColor: "#007fff",
-              borderWidth: 2,
-              borderRadius: 5,
+              margin: 10,
+              backgroundColor: "white",
               padding: 10,
             }}
+            key={index}
           >
-            <Text
+            <View
               style={{
-                textAlign: "center",
-                fontSize: 16,
-                fontWeight: "700",
-                color: "#007fff",
+                flexDirection: "row",
+                alignItems: "center",
+                justifyContent: "space-between",
               }}
             >
-              SELECT
+              <Text
+                style={{
+                  color: "#007fff",
+                  fontSize: 17,
+                  fontWeight: "500",
+                }}
+              >
+                {item.name}
+              </Text>
+              <AntDesign name="infocirlceo" size={24} color={"#007fff"} />
+            </View>
+
+            <Text style={{ marginTop: 3 }}>pay at property</Text>
+            <Text style={{ marginTop: 3, color: "green", fontSize: 16 }}>
+              Free cancellation Available
             </Text>
+            <View
+              style={{
+                marginTop: 4,
+                flexDirection: "row",
+                alignItems: "center",
+                gap: 10,
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 16,
+                  color: "red",
+                  textDecorationLine: "line-through",
+                }}
+              >
+                Rs{route.params.oldPrice}
+              </Text>
+              <Text
+                style={{
+                  fontSize: 18,
+                }}
+              >
+                Rs{route.params.newPrice}
+              </Text>
+            </View>
+            <Amenities />
+
+            {selected.includes(item.name) ? (
+              <Pressable
+                style={{
+                  borderColor: "#318CE7",
+                  backgroundColor: "#f0f8ff",
+                  borderWidth: 2,
+                  width: "100%",
+                  padding: 10,
+                  borderRadius: 5,
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <Text
+                  style={{
+                    textAlign: "center",
+                    marginLeft: "auto",
+                    marginRight: "auto",
+                    fontSize: 16,
+                    color: "#318CE7",
+                    fontWeight: "bold",
+                  }}
+                >
+                  SELECTED
+                </Text>
+                <Entypo
+                  onPress={() => setSelected([])}
+                  name="circle-with-cross"
+                  size={24}
+                  color="red"
+                />
+              </Pressable>
+            ) : (
+              <Pressable
+                onPress={() => setSelected(item.name)}
+                style={{
+                  borderColor: "#007fff",
+                  borderWidth: 2,
+                  borderRadius: 5,
+                  padding: 10,
+                }}
+              >
+                <Text
+                  style={{
+                    textAlign: "center",
+                    fontSize: 16,
+                    fontWeight: "700",
+                    color: "#007fff",
+                  }}
+                >
+                  SELECT
+                </Text>
+              </Pressable>
+            )}
           </Pressable>
+        ))}
+      </ScrollView>
+
+      {selected.length > 0 ? (
+        <Pressable
+          style={{
+            backgroundColor: "#007fff",
+            padding: 8,
+            marginBottom: 30,
+            borderRadius: 3,
+            marginHorizontal: 10,
+          }}
+        >
+          <Text
+            style={{ textAlign: "center", color: "white", fontWeight: "bold" }}
+          >
+            Reserve
+          </Text>
         </Pressable>
-      ))}
-    </ScrollView>
+      ) : null}
+    </>
   );
 };
 
