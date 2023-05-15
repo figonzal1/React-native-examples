aec6f6558ef27700cd84042cc9a68c3c73068507
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-05-15 15:44:31 -0400

    tailwind UI

--- TailwindProject/App.js
+++ TailwindProject/App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import tw, { useDeviceContext, useAppColorScheme } from "twrnc";
 
 export default function App() {
+  useDeviceContext(tw, { withDeviceColorScheme: false });
+
+  const [colorScheme, toggleColorScheme, setColorScheme] =
+    useAppColorScheme(tw);
+
+  useEffect(() => {
+    console.log(colorScheme);
+  }, [colorScheme]);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={tw`flex h-full items-center justify-center bg-cyan-50 dark:bg-gray-900`}
+    >
+      <Text style={tw`text-lg font-bold dark:text-white`}>New App</Text>
+      <Text style={tw`text-6xl p-3`}>
+        { colorScheme === 'dark' ? 'LUNA' : 'SOL'}
+      </Text>
+
+      <TouchableOpacity
+        onPress={toggleColorScheme}
+        style={tw`bg-cyan-600 py-3 px-6 rounded mt-10`}
+      >
+        <Text style={tw`text-white font-bold`}>My button</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
