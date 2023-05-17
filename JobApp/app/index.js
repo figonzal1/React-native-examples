151fd6ed6216a2dbdb5d4f35d86af4c78290c80e
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-05-17 18:55:12 -0400

    JobApp tutorial first commit

---
+++ JobApp/app/index.js
@@ -0,0 +1,12 @@
+import "expo-router/entry";
+import { Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
